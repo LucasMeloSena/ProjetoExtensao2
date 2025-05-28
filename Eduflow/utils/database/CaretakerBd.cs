ce9f66a1c59f42155c25d1ccdb6c1eed416e6809
Lucas Sena <93053816+LucasMeloSena@users.noreply.github.com>
2025-05-28 19:46:53 +0000

    feat: create caretaker data manipulation class

--- Eduflow/utils/database/CaretakerBd.cs
+++ Eduflow/utils/database/CaretakerBd.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Eduflow.models;
+using MySql.Data.MySqlClient;
 
 namespace Eduflow.utils.database
 {
-    internal class CaretakerBd
+    class CaretakerBd
     {
+        private database.Conn db;
+
+        public Caretaker getCaretaker(String userId)
+        {
+            db = new database.Conn();
+            using (var conn = new MySqlConnection(db.getConnectionString()))
+            {
+                conn.Open();
+                string query = "SELECT * FROM Cuidador where idUsuario = ?idUsuario";
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("?idUsuario", userId);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var id = reader["id"].ToString();
+                            var name = reader["nome"].ToString();
+                            var registration = reader["matricula"].ToString();
+                            return new Caretaker(id, name, registration, userId);
+                        }
+                        throw new Exception("Caretaker not found");
+                    }
+                }
+            }
+        }
     }
 }
